1e43cd9f33ee3ce4363cb84d949bfc67d757e11d
Lenny Peters <mr.lennypeters@gmail.com>
2017-06-16 10:56:14 -0700

    Master - added flyout menu via react

--- src/components/App.js
+++ src/components/App.js
@@ -2,44 +2,67 @@ import React, {Component} from 'react';
 import './App.scss';
 
 class App extends Component {
+    constructor(){
+        super();
+        this.state = {
+            condition: false
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick(e){
+        e.preventDefault();
+        this.setState({
+            condition: !this.state.condition
+        })
+    }
+    componentDidMount(){
+        let body = document.body;
+        body.classList.add('js');
+    }
     render() {
         return (
-            <div className="App">
-                <header>
-                    <nav>
-                        <ul>
-                            <li><a href="">Link</a></li>
-                        </ul>
-                    </nav>
-                </header>
-                <main id="main">
-                    <section>
-                        <h1>Headline One</h1>
-                        <div className="story">
-                            <h2>Headline Two</h2>
-                            <picture>
-                                <source srcset="http://lorempixel.com/300/169/city" media="(min-width: 500px)"/>
-                                <source srcset="http://lorempixel.com/480/270/city" media="(min-width: 700px)"/>
-                                <source srcset="http://lorempixel.com/640/360/city" media="(min-width: 900px)"/>
-                                <img src="http://lorempixel.com/720/405/city" className="img-responsive"
-                                     alt="lorempixel"/>
-                            </picture>
-                            <p>
-                                <cite>Some Author - </cite>
-                                Some other words and works
-                            </p>
-                        </div>
-                    </section>
-                    <aside>
-                        <h3>Headline Three</h3>
-                        <ul>
-                            <li>Some Thing</li>
-                        </ul>
-                    </aside>
-                </main>
-                <footer>
-                    <p className="center">Copyright 1980-{this.props.date.getFullYear()} ©</p>
-                </footer>
+            <div className="app-container">
+                <div className={this.state.condition ? 'wrap active' : 'wrap'} id="wrap">
+                    <header>
+                        <a href="#menu" className={this.state.condition ? 'menu-link active' : 'menu-link'} onClick={this.handleClick}>
+                            Menu &#9776;
+                        </a>
+                        <nav id="menu">
+                            <ul>
+                                <li><a href="">About</a></li>
+                                <li><a href="">Portfolio</a></li>
+                                <li><a href="">Services</a></li>
+                                <li><a href="">Contact</a></li>
+                            </ul>
+                        </nav>
+                    </header>
+                    <main id="content" className="main">
+                        <section>
+                            <h1>Headline One</h1>
+                            <div className="story">
+                                <h2>Headline Two</h2>
+                                <picture>
+                                    <source srcSet="https://placehold.it/400x300" media="(max-width:600px)"/>
+                                    <img src="https://placehold.it/705x405" className="img-responsive"
+                                         alt="placeholder"/>
+                                </picture>
+                                <p>
+                                    <cite>Some Author - </cite>
+                                    Some other words and works
+                                </p>
+                            </div>
+                        </section>
+                        <aside>
+                            <h3>Headline Three</h3>
+                            <ul>
+                                <li>Some Thing</li>
+                            </ul>
+                        </aside>
+                    </main>
+                    <footer>
+                        <p className="center">Copyright 1980-{this.props.date.getFullYear()} ©</p>
+                    </footer>
+                </div>
             </div>
         );
     }
