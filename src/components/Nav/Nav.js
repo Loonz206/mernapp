8ff3d7d7d4ea7c65d97a49321e617a27c6843587
Lenny Peters <mr.lennypeters@gmail.com>
2017-06-18 10:09:23 -0700

    Master - adding edits to the avigaiton, next to add routing

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,10 +4,12 @@ class Nav extends Component {
     render(){
         return (
             <nav id="menu">
-                <ul className="pull-left">
-                    <li><a href="">Lenny Peters</a></li>
+                <ul className="left-nav">
+                    <li><a href="">
+                        <img src="https://placehold.it/230x32" alt=""/>
+                    </a></li>
                 </ul>
-                <ul>
+                <ul className="right-nav">
                     <li><a href="">To-do Application</a></li>
                     <li><a href="">Login</a></li>
                     <li><a href="">Examples</a></li>
