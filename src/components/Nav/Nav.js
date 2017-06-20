d13c1997b9bc1d117d140d6eba04498b8909d897
Lenny Peters <lpeters@hollandamericagroup.com>
2017-06-20 10:41:38 -0700

    Master - added routing, 404 page, and also Google Analytics for the page, reducing nav items

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,14 +4,14 @@ class Nav extends Component {
     render(){
         return (
             <nav id="menu">
-                <ul className="left-nav">
-                    <li><a href="">React Playground</a></li>
-                </ul>
-                <ul className="right-nav">
-                    <li><a href="">To-do Application</a></li>
-                    <li><a href="">Chat</a></li>
-                    <li><a href="">Login</a></li>
-                </ul>
+                    <ul className="left-nav">
+                        <li><a href="">React Playground</a></li>
+                    </ul>
+                    <ul className="right-nav">
+                        <li><a href="">To-do Application</a></li>
+                        <li><a href="">Chat</a></li>
+                        <li><a href="">Login</a></li>
+                    </ul>
             </nav>
         )
     }
